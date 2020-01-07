7724a10c81e172bb6b610d225d07ee864c6daa1f
Tobias V. Langhoff <tobias@langhoff.no>
2020-01-07 23:56:03 +0100

    Add some XO-CHIP/SCHIP/quirks functionality

--- display.py
+++ display.py
@@ -7,7 +7,7 @@ SPACE = " "
 
 class BasicCursesDisplay:
     """CHIP-8 ncurses display"""
-    def __init__(self, stdscr):
+    def __init__(self, stdscr, quirks):
         self.active_planes = [0]
         self.width = 64
         self.height = 32
@@ -15,6 +15,8 @@ class BasicCursesDisplay:
         self.tall_mode = False
         self.hires_mode = False
         
+        self.quirks = quirks
+
         # Set the cursor as invisible as possible
         self.stdscr.leaveok(True)
         try:
@@ -27,7 +29,7 @@ class BasicCursesDisplay:
 
         self.stdscr.nodelay(True)
 
-        self.frame_buffer = [[[0] * self.width for _ in range(self.height)] for _ in range(3)]
+        self.frame_buffer = [[[0] * self.width for _ in range(self.height)]]
 
         self.plane_colors = [
             curses.COLOR_BLACK,
@@ -36,9 +38,8 @@ class BasicCursesDisplay:
             curses.COLOR_GREEN
         ]
 
-        #for pair in range(1,3):
-        #    curses.init_pair(pair, self.plane_colors[pair], curses.COLOR_BLACK)
-        #curses.init_pair(pair, self.plane_colors[4], curses.COLOR_BLACK)
+        #curses.init_pair(1, self.plane_colors[], curses.COLOR_BLACK)
+        #curses.init_pair(2, self.plane_colors[4], curses.COLOR_BLACK)
 
         self.check_display()
         self.clear()
@@ -66,6 +67,11 @@ class BasicCursesDisplay:
         self.check_display()
         self.hires_mode = False
         self.clear()
+    
+    def set_active_planes(self, planes):
+        if not self.frame_buffer[planes[-1]]:
+            self.frame_buffer += [[[0] * self.width for _ in range(self.height)] for __ in range(len(self.frame_buffer), planes[-1] - len(self.frame_buffer))]
+        self.active_planes = planes
 
     def clear(self):
         """Clear the display for any active planes."""
@@ -73,37 +79,22 @@ class BasicCursesDisplay:
             return
         for plane in self.active_planes:
             self.frame_buffer[plane] = [[0] * self.width for _ in range(self.height)]
-        if len(self.active_planes) != 2:
+        if len(self.active_planes) < len(self.frame_buffer):
+            # If only some of the existing planes are active, we'll need to redraw
+            # (unless we can smartly check that the inactive planes are all blank...)
             self.redraw()
         else:
             # If all planes are active, we just wipe the display instead of doing a costly
-            # redraw. TODO: This could also be done if we're not in XO mode at all.
+            # redraw.
             self.stdscr.erase()
             self.stdscr.refresh()
 
     def scroll(self, direction, pixels):
         pass
-    
-    def draw(self, x, y, sprite):
-        for row, num in enumerate(sprite):
-            if y + row > self.height:
-                break
-            for col in range(8):
-                if x + col > self.width:
-                    break
-                if (num >> (7 - col)) & 1:
-                    if self.stdscr.inch(y + row, x + col) & curses.A_CHARTEXT == 136: #BLOCK:
-                        collision = 1 # TODO QUIRK
-                        self.stdscr.addch(y + row, x + col, SPACE)
-                    else:
-                        self.stdscr.addch(y + row, x + col, BLOCK)
-
-        self.stdscr.refresh()
-        return collision
 
 class UnicodeCursesDisplay(BasicCursesDisplay):
-    def __init__(self, stdscr):
-        super().__init__(stdscr)
+    def __init__(self, stdscr, quirks):
+        super().__init__(stdscr, quirks)
     
     def check_display(self):
         """Assert that the display is large enoug."""
@@ -115,13 +106,7 @@ class UnicodeCursesDisplay(BasicCursesDisplay):
 
     def redraw(self):
         self.stdscr.erase()
-        if curses.LINES >= self.height and curses.COLS >= (self.width * 2) and not self.hires_mode:
-            for y in range(self.height):
-                for x in range(self.width):
-                    if self.frame_buffer[y][x]:
-                        self.stdscr.addch(y, (x * 2), BLOCK)
-                        self.stdscr.addch(y, (x * 2) + 1, BLOCK)
-        else:
+        if self.hires_mode or curses.LINES < self.height or curses.COLS < self.width:
             lookup = [
                 {
                     32:  ("▀", False),
@@ -138,9 +123,21 @@ class UnicodeCursesDisplay(BasicCursesDisplay):
             ]
             for y in range(self.height):
                 for x in range(self.width):
-                    if self.frame_buffer[y][x]:
-                        pixel = lookup[y % 2][self.stdscr.inch(y // 2, x) & curses.A_CHARTEXT]
-                        self.stdscr.addch(y // 2, x, pixel[0])
+                    for p in self.active_planes:
+                        if self.frame_buffer[p][y][x]:
+                            if self.frame_buffer[p][y][x]:
+                                pixel = lookup[y % 2][self.stdscr.inch(y // 2, x) & curses.A_CHARTEXT]
+                                self.stdscr.addch(y // 2, x, pixel[0])
+        else:
+            for y in range(self.height):
+                for x in range(self.width):
+                    for p in self.active_planes:
+                        if self.frame_buffer[p][y][x]:
+                            if curses.LINES >= self.height and curses.COLS >= (self.width * 2):
+                                self.stdscr.addch(y, (x * 2), BLOCK)
+                                self.stdscr.addch(y, (x * 2) + 1, BLOCK)
+                            elif curses.LINES >= self.height and curses.COLS >= self.width:
+                                self.stdscr.addch(y, x, BLOCK)
         self.stdscr.refresh()
 
     def draw(self, x, y, sprite):
@@ -148,65 +145,31 @@ class UnicodeCursesDisplay(BasicCursesDisplay):
         if not self.active_planes:
             return 0
 
-        lookup = [
-            {
-                32:  ("▀", False),
-                128: (" ", True), #upper
-                132: ("█", False),#lower
-                136: ("▄", True)    #full
-            },
-            {
-                32:  ("▄", False),
-                128: ("█", False), #upper
-                132: (" ", True), #lower
-                136: ("▀", True)    #full
-            }
-        ]
-
         collision = 0
         y = y % self.height
         x = x % self.width
 
-        if self.hires_mode and len(sprite) == 32:
+        if len(sprite) == 32 and (self.hires_mode or self.quirks['lores_wide_sprites']):
+            # FIXME Sprites can be 32 bytes large with the combination XO-CHIP, lores mode and DXY0!!!
             sprite_width = 16
             sprite = zip(sprite[0::2], sprite[1::2])
         else:
             sprite_width = 8
     
-        #for plane in range(active_plane):
-        
-        if curses.LINES >= self.height and curses.COLS >= (self.width * 2) and not self.hires_mode:
-            for row, num in enumerate(sprite):
-                if y + row > self.height:
-                    break
-                for col in range(8):
-                    if (x + col) * 2 > self.width * 2:
+        active_plane = cycle(self.active_planes)
+        for row, num in enumerate(sprite):
+            plane = self.frame_buffer[next(active_plane)]
+            if y + row > self.height:
+                break
+            if sprite_width == 16:
+                num = (num[0] << 8) | num[1]
+            for col in range(sprite_width):
+                    if x + col > self.width:
                         break
-                    if (num >> (7 - col)) & 1:
-                        self.frame_buffer[y + row][x + col] ^= 1
-                        if self.stdscr.inch(y + row, (x * 2) + (col * 2)) & curses.A_CHARTEXT == 136: #BLOCK:
-                            collision = 1 # TODO QUIRK
-                            self.stdscr.addch(y + row, (x * 2) + (col * 2), SPACE)
-                            self.stdscr.addch(y + row, (x * 2) + (col * 2) + 1, SPACE)
-                            
-                        else:
-                            self.stdscr.addch(y + row, (x * 2) + (col * 2), BLOCK)
-                            self.stdscr.addch(y + row, (x * 2) + (col * 2) + 1, BLOCK)
-        else:
-            for row, num in enumerate(sprite):
-                if y + row > self.height:
-                    break
-                if sprite_width == 16:
-                    num = (num[0] << 8) | num[1]
-                for col in range(sprite_width):
                     if (num >> (sprite_width - col - 1)) & 1:
-                        self.frame_buffer[y + row][x + col] ^= 1
-                        if x + col > self.width:
-                            break
-                        pixel = lookup[(y + row) % 2][self.stdscr.inch((y + row) // 2, x + col) & curses.A_CHARTEXT]
-                        self.stdscr.addch((y + row) // 2, x + col, pixel[0])
-                        if pixel[1]:
+                        if plane[y + row][x + col]:
                             collision = 1
+                        plane[y + row][x + col] ^= 1
 
-        self.stdscr.refresh()
+        self.redraw() # TODO Change this to just redraw affected area
         return collision
